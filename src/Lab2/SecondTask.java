69c350a83632bfb795e7279c553f56e8d705424d
BoulevardOfBrokenDreams <eplyago@mail.ru>
2022-09-22 10:08:07 +0300

    Task 2 final

--- src/Lab2/SecondTask.java
+++ src/Lab2/SecondTask.java
@@ -1,8 +1,9 @@
 package Lab2;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class SecondTask {
+    //14 13 12 11 10 9 8 7 15 6 5 4 3 2 1
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter m parameter: ");
@@ -10,16 +11,63 @@ public class SecondTask {
         System.out.print("Enter n parameter: ");
         var n = scanner.nextInt();
         System.out.print("Enter the array: ");
-        int[][] array = new int[m][n];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        var matrix = enterMatrix(m,n);
+
+        printMatrix(matrix);
+
+        var maximalInMatrix = findMax(matrix);
+        var lastStringNumber = lastStringWithElement(matrix, maximalInMatrix);
+
+        if(lastStringNumber != -1)
+            m -= 1;
+
+        int[][] newMatrix = new int[m][n];
+        for(int i = 0, j = 0; i < matrix.length; i++){
+            if(i == lastStringNumber){
+                continue;
+            }
+            newMatrix[j] = matrix[i];
+            j++;
+        }
+
+        System.out.println();
+        printMatrix(newMatrix);
+
+        Vector<Integer> vectorB = new Vector<Integer>();
+
+        for(int i = 0; i < newMatrix.length; i++){
+            for(int j = 0; j < newMatrix[i].length; j++){
+                vectorB.add(newMatrix[i][j]);
+            }
+        }
+
+        Collections.sort(vectorB);
+        System.out.println(vectorB);
+    }
+
+    public static int lastStringWithElement(int[][] array, int necessaryItem ){
+        var stringNumber = -1;
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                if(array[i][j] == necessaryItem){
+                    stringNumber = i;
+                }
+            }
+        }
+
+        return stringNumber;
+    }
+
+    public static  int[][] enterMatrix(int firstIndex, int secondIndex){
+        Scanner scanner = new Scanner(System.in);
+        int[][] array = new int[firstIndex][secondIndex];
+        for(int i = 0; i < firstIndex; i++){
+            for(int j = 0; j < secondIndex; j++){
                 array[i][j] = scanner.nextInt();
             }
         }
 
-        printMatrix(array);
-
-        System.out.println(findMax(array));
+        return array;
     }
 
     public static void printMatrix(int[][] array){
@@ -27,12 +75,11 @@ public class SecondTask {
             for(int j = 0; j < array[i].length; j++){
                 System.out.print(array[i][j] + "\t");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 
     public static int findMax(int[][] array){
-
         var maximal = Integer.MIN_VALUE;
         for(int i = 0; i < array.length; i++){
             for(int j = 0; j < array[0].length; j++){
@@ -41,6 +88,7 @@ public class SecondTask {
                 }
             }
         }
+
         return maximal;
     }
 }
